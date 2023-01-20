4ef3959bf979539931c6ae71d69f09f2b9bfe890
Branches7 <50388052+Branches7@users.noreply.github.com>
2023-01-20 00:31:16 -0300

    Update changelog5.sf.sql

--- scripts/deploy/changelog5.sf.sql
+++ scripts/deploy/changelog5.sf.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS employees (
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     department VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+)
 
 CREATE TABLE IF NOT EXISTS departments (
     department_id INT PRIMARY KEY,
     department_name VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+)
 

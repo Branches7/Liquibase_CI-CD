46bd99b18a5092c126b4404d19abf729cacc13c5
Branches7 <50388052+Branches7@users.noreply.github.com>
2023-01-19 23:04:43 -0300

    Add files via upload

---
+++ scripts/deploy/changelog3.sf.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset branches:4
+
+use database DEMO_DB;
+use schema liquibase;
+create or replace table employer (
+    id int primary key,
+    name varchar(50) not null
+)

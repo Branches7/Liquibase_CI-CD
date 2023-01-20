32eb74cdea2b66586778664f7210d148b002da00
Branches7 <50388052+Branches7@users.noreply.github.com>
2023-01-20 00:23:55 -0300

    Add files via upload

---
+++ scripts/deploy/changelog5.sf.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset branches:
+use database DEMO_DB;
+use schema liquibase;
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    department VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+

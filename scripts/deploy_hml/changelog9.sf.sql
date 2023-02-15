9052834550fdd9878fa655843b205973f78dce7e
Branches7 <50388052+Branches7@users.noreply.github.com>
2023-02-14 22:16:55 -0300

    Update changelog9.sf.sql

--- scripts/deploy_hml/changelog9.sf.sql
+++ scripts/deploy_hml/changelog9.sf.sql
@@ -3,8 +3,6 @@
 --changeset branches:2
 --comment: test 2
 
-use database DEMO_DB;
-use schema liquibase;
 create or replace table tb_test_3 (
     id int primary key,
     name varchar(50) not null
